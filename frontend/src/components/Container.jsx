c7be77640b211c9fa79775917d865b84447543f0
Mishan Poudel <poudelmishan2@gmail.com>
2024-03-03 21:45:43 +0000

    style: establish initial file explorer structure

---
+++ frontend/src/components/Container.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Container() {
+  return (
+    <>
+        <div className='absolute bg-blue-400 top-[10px] h-[1.8em] left-[10px] w-60 rounded-t-lg'>
+            this iswhat it is
+        </div>
+        <div className='bg-green-400 w-full h-11'>
+            jhsdf
+        </div>
+        <div className='bg-blue-400 w-full h-[3.5rem]'>
+            jhsdf
+        </div>
+        <div className='flex flex-row h-full'>
+          <div className='bg-yellow-500 border-r-2 w-40 h-full mt-2'>this is a sidebar</div>
+          <div className='bg-blue-500 border-0 ml-5 mt-2'>this is content area</div>
+        </div>
+
+          <div className='absolute bottom-0 h-5 bg-white w-full text-xs py-1'> 
+          <div className='flex items-center justify-center w-16 border-r-2  h-full'>
+            5 items
+          </div>
+          </div>
+    </>
+  )
+}
---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Title = ({ constraintsRef, children }) => {
+  return (
+    <motion.div
+      className="container bg-black w-[69em] relative left-[28.5em] top-40 h-[40em] rounded-xl overflow-hidden"
+      ref={constraintsRef}
+      drag
+      dragConstraints={constraintsRef}
+      dragTransition={{ velocity: 1 }}
+    >
+      <div className="h-10 flex justify-end items-center overflow-hidden">
+        <span class="material-symbols-outlined btn text-lg bg-transparent border-0">minimize</span>
+        <span class="material-symbols-outlined btn text-lg bg-transparent border-0">check_box_outline_blank</span>
+        <span class="material-symbols-outlined btn text-xl hover:bg-red-700 bg-transparent border-0">close</span>
+      </div>
+      {children}
+    </motion.div>
+  );
+};
+
+export default Title;
