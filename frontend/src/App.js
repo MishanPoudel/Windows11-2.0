120b779c552bffdc1cbb9f8f91b172c458f05fff
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-03-14 21:55:18 +0000

    fix: Resolve authorization and routing bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
-import axios from "axios";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { AuthContext, AuthContextProvider } from "./Context/AuthContext";
 import Lockscreen from "./Pages/lockscreen";
 import Main from "./Pages/main";
+import axios from "axios";
 
 axios.defaults.withCredentials = true;
 function App() {
+  const { loggedIn } = React.useContext(AuthContext);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/lockscreen" element={<Lockscreen />} />
-        <Route path="/main" element={<Main />} />
-      </Routes>
-    </Router>
+    <AuthContextProvider>
+      <Router>
+        <Routes>
+          {loggedIn === false && <Route path="*" element={<Lockscreen />} />}
+          {loggedIn === true && <Route path="/main" element={<Main />} />}
+        </Routes>
+      </Router>
+    </AuthContextProvider>
   );
 }
 
