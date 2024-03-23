e8ca36dbbe441b9bbb9867d57cc06a26b50b87b2
Mishan Poudel <92642350+MishanPoudel@users.noreply.github.com>
2024-03-23 22:21:46 +0000

    feat: Window size check & error handling (small screens)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./Context/AuthContext";
 
+const AppContainer = () => {
+  const [isLG, setIsLG] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLG(window.innerWidth >= 1024); // Adjust the width according to the lg breakpoint in Tailwind CSS
+    };
+
+    // Initial check on component mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup function to remove event listener
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <AuthContextProvider>
+      <React.StrictMode>
+        {isLG ? (
+          <App />
+        ) : (
+          <div className="h-screen w-full flex justify-center items-center bg-black px-12">
+            <div className="text-2xl">Not available in smaller screens for now.</div>
+          </div>
+        )}
+      </React.StrictMode>
+    </AuthContextProvider>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <AuthContextProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </AuthContextProvider>
-);
+root.render(<AppContainer />);
